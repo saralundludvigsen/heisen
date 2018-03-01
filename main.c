858e49a1dedb1712a5a4526bd935558372581ae4
Sara Lund Ludvigsen <sarall@stud.ntnu.no>
2018-03-01 14:22:34 +0100

    prøver å fikse litt i main

--- main.c
+++ main.c
@@ -28,17 +28,17 @@ int main() {
 	while (1) {
 		//sjekker hele tiden om og hvilken knapp som er trykket
 		//og setter køen vha add_to_queue()
-		if (elev_get_button_signal(BUTTON_CALL_UP, 1) == 1) {
-			elev_set_motor_direction(DIRN_UP);
-		}
+
 		if (elev_get_stop_signal() == 1) {
 			event_emergency_stop_pushed();
 		}
 		
 		for(int i = 0; i < N_FLOORS; i++){
 			for(int j = 0; j < N_BUTTONS; j++){
-				if (elev_get_button_signal(static_cast<elev_button_type_t>(j), i)){
-					event_button_pushed(i, static_cast<button_type>(j));
+				elev_button_type_t emma = (elev_button_type_t) j;
+				button_type sara = (button_type) j;
+				if (elev_get_button_signal(emma, i)){
+					event_button_pushed(i, (sara));
 				}
 			}
 		}
@@ -53,7 +53,7 @@ int main() {
 		else if (queue_is_empty()){
 			event_queue_is_empty();
 		}
-
+		
 		else if(reached_floor_to_stop_in()){
 			event_reached_floor();
 		}
