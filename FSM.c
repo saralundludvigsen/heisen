480b5fab7bd0ae2667d65a768e6bb868a020f10c
Sara Lund Ludvigsen <sarall@stud.ntnu.no>
2018-03-01 09:02:36 +0100

    kjør opp

--- FSM.c
+++ FSM.c
@@ -22,6 +22,7 @@ void event_emergency_stop_pushed() {
 }
 
 void event_button_pushed(int floor, button_type button) {
+    elev_set_motor_direction(DIRN_UP);
 	switch (state) {
 
 	case (emergency_stop):
