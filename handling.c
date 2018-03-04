695aca419e4cea9dae8b7273baa78a2091d68436
Sara Lund Ludvigsen <sarall@stud.ntnu.no>
2018-03-04 16:09:33 +0100

    bytter get_direction

--- handling.c
+++ handling.c
@@ -36,7 +36,7 @@ bool reached_floor_to_stop_in(elev_motor_direction_t current_direction) {
 	}
 
 }
-
+/*
 elev_motor_direction_t get_direction(int floor_in, int floor_to) {
 	if (floor_in < floor_to) {
 		return DIRN_UP;
@@ -47,51 +47,51 @@ elev_motor_direction_t get_direction(int floor_in, int floor_to) {
 	else {
 		return DIRN_STOP;
 	}
-}
+}*/
 
-//elev_motor_direction_t get_direction(int prev_floor, elev_motor_direction_t curr_dir) {
-	//if (curr_dir == DIRN_UP) {
-	//	for (int i = prev_floor; i < N_FLOORS; i++) {
-	//		//har bestilling til etasje over den den er i og er på vei opp
-	//		if ((is_order(BUTTON_UP, i) || is_order(BUTTON_COM, i))) {
-	//			return DIRN_UP;
-	//		}
-	//	}
-	//	//har IKKE best. til over den den er i:
-	//	//sjekker om den har bestillinger under:
-	//	for (int i = 0; i < prev_floor; i++) {
-	//		if (is_order(BUTTON_DOWN, i) || is_order(BUTTON_COM, i)) {
-	//			return DIRN_DOWN;
-	//		}
-	//	}
-	//}
-	//else if (curr_dir == DIRN_DOWN) {
-	//	//sjekker om best. til UNDER den den er i:
-	//	for (int i = 0; i < prev_floor; i++) {
-	//		if (is_order(BUTTON_DOWN, i) || is_order(BUTTON_COM, i)) {
-	//			return DIRN_DOWN;
-	//		}
-	//	}
-	//	for (int i = prev_floor; i < N_FLOORS; i++) {
-	//		//hvis ikke, har den bestilling over?
-	//		if ((is_order(BUTTON_UP, i) || is_order(BUTTON_COM, i))) {
-	//			return DIRN_UP;
-	//		}
-	//	}
-	//}
-	//else if (curr_dir == DIRN_STOP) {
-	//	//køen har vært tom, men har nå én bestilling.
-	//	for (int i = 0; i < N_FLOORS; i++) {
-	//		if (is_order(BUTTON_DOWN, i) || is_order(BUTTON_UP, i) || is_order(BUTTON_COM, i)) {
-	//			if (prev_floor < i) {
-	//				return DIRN_UP;
-	//			}
-	//			else if (prev_floor > i) {
-	//				return DIRN_DOWN;
-	//			}
-	//		}
-	//	}
-	//}
-	//ta seg av andre merkelige tilfeller
-	//return DIRN_STOP;
-//}
+elev_motor_direction_t get_direction(int prev_floor, elev_motor_direction_t curr_dir) {
+	if (curr_dir == DIRN_UP) {
+		for (int i = prev_floor; i < N_FLOORS; i++) {
+			//har bestilling til etasje over den den er i og er på vei opp
+			if ((is_order(BUTTON_UP, i) || is_order(BUTTON_COM, i))) {
+				return DIRN_UP;
+			}
+		}
+		//har IKKE best. til over den den er i:
+		//sjekker om den har bestillinger under:
+		for (int i = 0; i < prev_floor; i++) {
+			if (is_order(BUTTON_DOWN, i) || is_order(BUTTON_COM, i)) {
+				return DIRN_DOWN;
+			}
+		}
+	}
+	else if (curr_dir == DIRN_DOWN) {
+		//sjekker om best. til UNDER den den er i:
+		for (int i = 0; i < prev_floor; i++) {
+			if (is_order(BUTTON_DOWN, i) || is_order(BUTTON_COM, i)) {
+				return DIRN_DOWN;
+			}
+		}
+		for (int i = prev_floor; i < N_FLOORS; i++) {
+			//hvis ikke, har den bestilling over?
+			if ((is_order(BUTTON_UP, i) || is_order(BUTTON_COM, i))) {
+				return DIRN_UP;
+			}
+		}
+	}
+	else if (curr_dir == DIRN_STOP) {
+		//køen har vært tom, men har nå én bestilling.
+		for (int i = 0; i < N_FLOORS; i++) {
+			if (is_order(BUTTON_DOWN, i) || is_order(BUTTON_UP, i) || is_order(BUTTON_COM, i)) {
+				if (prev_floor < i) {
+					return DIRN_UP;
+				}
+				else if (prev_floor > i) {
+					return DIRN_DOWN;
+				}
+			}
+		}
+	}
+	ta seg av andre merkelige tilfeller
+	return DIRN_STOP;
+}
